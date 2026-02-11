2ea5312e3104b1c4e79f73e91a6577068feb1bd2
Jorge Reina <jorgereina.fl@gmail.com>
2026-02-11 20:59:17 +0100

    feat: add Google Tag tracking script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -67,6 +67,21 @@ export default function RootLayout({
                     strategy="beforeInteractive"
                 />
 
+                {/* Google Tag (gtag.js) */}
+                <Script
+                    src="https://www.googletagmanager.com/gtag/js?id=G-1PEGZXZXV2"
+                    strategy="afterInteractive"
+                />
+                <Script id="google-analytics" strategy="afterInteractive">
+                    {`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+
+                        gtag('config', 'G-1PEGZXZXV2');
+                    `}
+                </Script>
+
                 <Header />
                 <div className="mx-auto max-w-screen-2xl">
                     <ClientLayout>
